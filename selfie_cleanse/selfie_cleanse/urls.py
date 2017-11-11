c45f50e73e2e0f397c6082de741431642f7986c1
stephxu96 <scarletxuiam0917@gmail.com>
2017-11-11 10:41:28 -0500

    Fixed admin page

--- selfie_cleanse/selfie_cleanse/urls.py
+++ selfie_cleanse/selfie_cleanse/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from rest_framework import routers, serializers, viewsets
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^admin/', include(admin.site.urls)),
 ]
