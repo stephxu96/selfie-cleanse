4974b42f823766ff4e6521a603a75e4538f23edb
bp9614 <bpham9614@gmail.com>
2017-11-12 12:17:48 -0500

    Update

--- selfie_cleanse/pictures/views.py
+++ selfie_cleanse/pictures/views.py
@@ -4,14 +4,15 @@ from PIL import Image
 from pictures.selfie_recognition import SelfieDetection
 import base64
 import io
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 selfie_detection = SelfieDetection()
 
+@csrf_exempt
 def upload_file(request):
     if request.method == 'POST':
-        print(request)
-        image = request.form['img']
+        image = request.FILES['image']
         if selfie_detection.is_selfie(io.BytesIO(base64.b64decode(image))):
             return HttpResponse("Is a selfie")
         else:
